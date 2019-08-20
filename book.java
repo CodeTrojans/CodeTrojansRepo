4dbe2f92a4e29666cbb8d2b64b864167aecedb97
RammySekham <49733815+RammySekham@users.noreply.github.com>
2019-08-20 17:03:35 +1000

    Iteration1
    Iteration1 Completed. Ready for code review

--- book.java
+++ book.java
@@ -1,96 +1,84 @@
 import java.io.Serializable;
-
-
+//File name changed to 'book' to 'Book'
 @SuppressWarnings("serial")
-public class book implements Serializable {
-	
-	private String TITLE;
-	private String AUTHOR;
-	private String CALLNO;
-	private int ID;
-	
-	private enum STATE { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
-	private STATE State;
-	
-	
-	public book(String author, String title, String callNo, int id) {
-		this.AUTHOR = author;
-		this.TITLE = title;
-		this.CALLNO = callNo;
-		this.ID = id;
-		this.State = STATE.AVAILABLE;
+public class Book implements Serializable {// 'Class name changed from 'book' to 'Book'
+
+	private String bookTitle;// 'TITLE' is changed to 'bookTitle'
+	private String bookAuthor; // 'AUTHOR' is changed to 'bookauthor'
+	private String bookCallNo; // 'CALLNO' is changed to 'bookCallNo'
+	private int bookId;// 'ID'is changed to 'bookId'
+
+	private enum State {
+		BOOK_AVAILABLE, BOOK_ON_LOAN, BOOK_DAMAGED, BOOK_RESERVED
+	};// 'all constants are changed as per guidelines'
+
+	private State bookState;// 'state variable is changed to bookState'
+
+	public Book(String author, String title, String callNo, int id) {
+		this.bookAuthor = author;
+		this.bookTitle = title;// 'TITLE' is changed to 'bookTitle'
+		this.bookCallNo = callNo;// 'CALLNO' is changed to 'bookCallNo'
+		this.bookId = id;// 'ID'is changed to 'bookId'
+		this.bookState = State.BOOK_AVAILABLE;// 'AVAIALABLE' is changed to BOOK_AVAILABLE
 	}
-	
+
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
-		sb.append("Book: ").append(ID).append("\n")
-		  .append("  Title:  ").append(TITLE).append("\n")
-		  .append("  Author: ").append(AUTHOR).append("\n")
-		  .append("  CallNo: ").append(CALLNO).append("\n")
-		  .append("  State:  ").append(State);
-		
+		sb.append("Book: ").append(bookId).append("\n")// 'ID'is changed to 'bookId'
+				.append("  Title:  ").append(bookTitle).append("\n")// 'TITLE' is changed to 'bookTitle'
+				.append("  Author: ").append(bookAuthor).append("\n")// 'AUTHOR' is changed to 'bookauthor'
+				.append("  CallNo: ").append(bookCallNo).append("\n")// 'CALLNO' is changed to 'bookCallNo'
+				.append("  State:  ").append(bookState);// 'state variable is changed to bookState'
+
 		return sb.toString();
 	}
 
-	public Integer ID() {
-		return ID;
+	public int getbookId() {// 'ID' is changed to 'getbookId'
+		return bookId;// 'ID'is changed to 'bookId'
 	}
 
-	public String TITLE() {
-		return TITLE;
+	public String getbookTitle() {// 'TITLE' is changed to 'getbookTitle'
+		return bookTitle;// 'TITLE' is changed to 'bookTitle'
 	}
 
-
-	
-	public boolean AVAILABLE() {
-		return State == STATE.AVAILABLE;
+	public boolean isAvaialble() {// 'AVAILABLE' IS CHANGED TO 'isAvaiable'
+		return bookState == State.BOOK_AVAILABLE; // 'State' is changed to 'bookState'
 	}
 
-	
-	public boolean On_loan() {
-		return State == STATE.ON_LOAN;
+	public boolean isOnLoan() {// 'On_loan' IS CHANGED TO 'isOnLoan'
+		return bookState == State.BOOK_ON_LOAN;// 'State' is changed to 'bookState'
 	}
 
-	
-	public boolean IS_Damaged() {
-		return State == STATE.DAMAGED;
+	public boolean isDamaged() {// 'DAMAGED' IS CHANGED TO 'isdamaged'
+		return bookState == State.BOOK_DAMAGED;// 'State' is changed to 'bookState'
 	}
 
-	
-	public void Borrow() {
-		if (State.equals(STATE.AVAILABLE)) {
-			State = STATE.ON_LOAN;
-		}
-		else {
-			throw new RuntimeException(String.format("Book: cannot borrow while book is in state: %s", State));
-		}
-		
+	public void isBorrowed() {// 'BORROWED' IS CHANGED TO 'isBorrowed'
+		if (bookState.equals(State.BOOK_AVAILABLE)) {// 'State' is changed to 'bookState'
+			bookState = State.BOOK_ON_LOAN;// 'State' is changed to 'bookState'
+		} else {
+			throw new RuntimeException(String.format("Book: cannot borrow while book is in state: %s", bookState));
+		} // 'State' is changed to 'bookState'
+
 	}
 
-
-	public void Return(boolean DAMAGED) {
-		if (State.equals(STATE.ON_LOAN)) {
-			if (DAMAGED) {
-				State = STATE.DAMAGED;
+	public void isReturned(boolean isDamaged) {// RETURED is changed to 'isRetured'
+		if (bookState.equals(State.BOOK_ON_LOAN)) {
+			if (isDamaged) {
+				bookState = State.BOOK_DAMAGED;// 'State' is changed to 'bookState'
+			} else {
+				bookState = State.BOOK_AVAILABLE;// 'State' is changed to 'bookState'
 			}
-			else {
-				State = STATE.AVAILABLE;
-			}
-		}
-		else {
-			throw new RuntimeException(String.format("Book: cannot Return while book is in state: %s", State));
-		}		
+		} else {
+			throw new RuntimeException(String.format("Book: cannot Return while book is in state: %s", bookState));
+		} // 'State' is changed to 'bookState'
 	}
 
-	
-	public void Repair() {
-		if (State.equals(STATE.DAMAGED)) {
-			State = STATE.AVAILABLE;
-		}
-		else {
-			throw new RuntimeException(String.format("Book: cannot repair while book is in state: %s", State));
-		}
+	public void isRepaired() {// Repaired is changed to 'isRepaired'
+		if (bookState.equals(State.BOOK_DAMAGED)) {// 'State' is changed to 'bookState'
+			bookState = State.BOOK_AVAILABLE;// 'State' is changed to 'bookState'
+		} else {
+			throw new RuntimeException(String.format("Book: cannot repair while book is in state: %s", bookState));
+		} // 'State' is changed to 'bookState'
 	}
-
-
 }
