3e9b2c52b3c4118dc9fe7a1bf44405b64a102f33
NagendraPonnaganti <53213720+NagendraPonnaganti@users.noreply.github.com>
2019-08-23 11:44:27 +1000

    Initial commit on  ReturnBookControl.java
    Initial commit on  ReturnBookControl.java 'Ui' is changed to 'ui'

--- ReturnBookControl.java
+++ ReturnBookControl.java
@@ -1,6 +1,6 @@
 public class ReturnBookControl {
 
-	private ReturnBookUI Ui;
+	private ReturnBookUI Ui; //  'Ui' is changed to 'ui'
 	private enum CONTROL_STATE { INITIALISED, READY, INSPECTING };
 	private CONTROL_STATE sTaTe;
 	
@@ -18,7 +18,7 @@ public class ReturnBookControl {
 		if (!sTaTe.equals(CONTROL_STATE.INITIALISED)) {
 			throw new RuntimeException("ReturnBookControl: cannot call setUI except in INITIALISED state");
 		}	
-		this.Ui = ui;
+		this.ui = ui; //  'Ui' is changed to 'ui'
 		ui.Set_State(ReturnBookUI.UI_STATE.READY);
 		sTaTe = CONTROL_STATE.READY;		
 	}
@@ -31,11 +31,11 @@ public class ReturnBookControl {
 		book CUR_book = lIbRaRy.Book(Book_ID);
 		
 		if (CUR_book == null) {
-			Ui.display("Invalid Book Id");
+			ui.display("Invalid Book Id"); //  'Ui' is changed to 'ui'
 			return;
 		}
 		if (!CUR_book.On_loan()) {
-			Ui.display("Book has not been borrowed");
+			ui.display("Book has not been borrowed"); //  'Ui' is changed to 'ui'
 			return;
 		}		
 		CurrENT_loan = lIbRaRy.LOAN_BY_BOOK_ID(Book_ID);	
@@ -43,14 +43,14 @@ public class ReturnBookControl {
 		if (CurrENT_loan.OVer_Due()) {
 			Over_Due_Fine = lIbRaRy.CalculateOverDueFine(CurrENT_loan);
 		}
-		Ui.display("Inspecting");
-		Ui.display(CUR_book.toString());
-		Ui.display(CurrENT_loan.toString());
+		ui.display("Inspecting"); //  'Ui' is changed to 'ui'
+		ui.display(CUR_book.toString()); //  'Ui' is changed to 'ui'
+		ui.display(CurrENT_loan.toString()); //  'Ui' is changed to 'ui'
 		
 		if (CurrENT_loan.OVer_Due()) {
-			Ui.display(String.format("\nOverdue fine : $%.2f", Over_Due_Fine));
+			ui.display(String.format("\nOverdue fine : $%.2f", Over_Due_Fine)); //  'Ui' is changed to 'ui'
 		}
-		Ui.Set_State(ReturnBookUI.UI_STATE.INSPECTING);
+		ui.Set_State(ReturnBookUI.UI_STATE.INSPECTING); //  'Ui' is changed to 'ui'
 		sTaTe = CONTROL_STATE.INSPECTING;		
 	}
 
@@ -72,6 +72,4 @@ public class ReturnBookControl {
 		Ui.Set_State(ReturnBookUI.UI_STATE.READY);
 		sTaTe = CONTROL_STATE.READY;				
 	}
-
-
 }
